640471ca5610f2f9d27b92ea0b2ff9223e8e803a
Maxiwell Luo <maxluoxiii@gmail.com>
2022-02-14 18:30:29 -0600

    Finish day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,20 +6,27 @@ use std::collections::HashMap;
 fn main() {
     let input_file = File::open("inputs/day10.txt").expect("Unable to open file");
 
-    let score_map = HashMap::from([
+    let syntax_score_map = HashMap::from([
         (')', 3),
         (']', 57),
         ('}', 1197),
         ('>', 25137)
     ]);
+    let mut syntax_score = 0;
 
-
-    let mut total_score = 0;
+    let autocomp_score_map = HashMap::from([
+        ('(', 1),
+        ('[', 2),
+        ('{', 3),
+        ('<', 4)
+    ]);
+    let mut autocomp_scores = Vec::new();
     
     for line in io::BufReader::new(input_file).lines() {
         let line = line.unwrap();
 
         let mut c_stack = Vec::new();
+        let mut corrupt = false;
         for c in line.chars() {
             match c {
                 '(' => c_stack.push(c),
@@ -29,7 +36,8 @@ fn main() {
                 ')' => {
                     if let Some(pop_c) = c_stack.pop() {
                         if '(' != pop_c {
-                            total_score += score_map[&c];
+                            syntax_score += syntax_score_map[&c];
+                            corrupt = true;
                             break;
                         }
                     }
@@ -37,7 +45,8 @@ fn main() {
                 ']' => {
                     if let Some(pop_c) = c_stack.pop() {
                         if '[' != pop_c {
-                            total_score += score_map[&c];
+                            syntax_score += syntax_score_map[&c];
+                            corrupt = true;
                             break;
                         }
                     }
@@ -45,7 +54,8 @@ fn main() {
                 '}' => {
                     if let Some(pop_c) = c_stack.pop() {
                         if '{' != pop_c {
-                            total_score += score_map[&c];
+                            syntax_score += syntax_score_map[&c];
+                            corrupt = true;
                             break;
                         }
                     }
@@ -53,7 +63,8 @@ fn main() {
                 '>' => {
                     if let Some(pop_c) = c_stack.pop() {
                         if '<' != pop_c {
-                            total_score += score_map[&c];
+                            syntax_score += syntax_score_map[&c];
+                            corrupt = true;
                             break;
                         }
                     }
@@ -61,7 +72,27 @@ fn main() {
                 _ => {}
             };
         }
+
+        if !corrupt && !c_stack.is_empty() {
+            let mut autocomp_score: u64 = 0;
+            while !c_stack.is_empty() {
+                if let Some(pop_c) = c_stack.pop() {
+                    match pop_c {
+                        '(' => autocomp_score = autocomp_score * 5 + autocomp_score_map[&pop_c],
+                        '[' => autocomp_score = autocomp_score * 5 + autocomp_score_map[&pop_c],
+                        '{' => autocomp_score = autocomp_score * 5 + autocomp_score_map[&pop_c],
+                        '<' => autocomp_score = autocomp_score * 5 + autocomp_score_map[&pop_c],
+                        _ => {}
+                    };
+                }
+            }
+            autocomp_scores.push(autocomp_score);
+        }
     }
 
-    println!("Total score: {total_score}");
+    autocomp_scores.sort_unstable();
+    let middle_score = autocomp_scores[autocomp_scores.len() / 2];
+
+    println!("Syntax score: {syntax_score}");
+    println!("Autocomplete score: {middle_score}");
 }
